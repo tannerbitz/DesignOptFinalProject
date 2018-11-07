f2b1b76f1b8566d85432e96b3a334fb34d08c639
Tanner Bitz <tannerbitz@gmail.com>
2018-11-06 22:11:52 -0700

    added Fiala Tire Model

--- Code/Fiala.m
+++ Code/Fiala.m
@@ -1,28 +1,80 @@
-classdef Fiala
+classdef Fiala < handle
     %FIALA Summary of this class goes here
     %   Detailed explanation goes here
     
     properties
         radiusWheel    % wheel radius
-        C           % coefficient of friction (Cr = Cf = C)
+        C              % tire stiffness (Cr = Cf = C)
+        mu             % coefficient of friction
     end
     
     methods
-        function obj = Fiala(radiusWheel_, C_)
+        function obj = Fiala()
             %FIALA Construct an instance of this class
             %   Detailed explanation goes here
-            obj.radiusWheel = radiusWheel_;
-            obj.C = C_;
         end
         
-        function  fLong = getFLong(obj)
-            %getFLong calculates the longitudinal foce on the tire
-            fLong = something;
+        function obj = setupfiala(obj, varargin)
+            % Fiala property defaults
+            defaultRadiusWheel = 1; %m
+            defaultC = 1;           %unitless
+            
+            % Set Parser/Parse Inputs
+            p = inputParser;
+            addParameter(p, 'radiusWheel', defaultRadiusWheel);
+            addParameter(p, 'C', defaultC);
+            addParameter(p, 'mu', defaultMu);
+            parse(p, varargin{:});
+            
+            % Set object properties
+            obj.massVehicle = p.Results.radiusWheel;
+            obj.vx = p.Results.C;
+            obj.mu = p.Results.mu;
         end
         
-        function fLat = getFLat(obj)
-            %getFLat calculates the lateral force on the a tire
-            fLat = something;
+        function  [fLong, fLat] = getTireForces(obj, vehicleObj, Fz, accelBrakeFlag)
+            %getTireForces calculates and returns the latitudinal and
+            %longitudinal forces on a wheel. The inputs are the normal
+            %force on the tire, Fz, and a flag that specifies whether the
+            %car is accelerating or braking. 
+            
+            % Parse Inputs
+            p = inputParser;
+            addRequired(p, 'vehicleObj');
+            addRequired(p, 'Fz');
+            addRequired(p, 'accelBrakeFlag');
+            parse(p, vehicleObj, Fz, accelBrakeFlag);
+            
+            vObjTemp = p.Results.vehicleObj;
+            fzTemp = p.Results.Fz;
+            accelBrakeFlag = p.Results.accelBrakeFlag;
+            
+            % Calculate sigmaLat/ sigmaLong/ sigma
+            if strcmp(accelBrakeFlag, 'accel')
+                sigmaLat = (obj.radiusWheel*vObjTemp.omegaW - vObjTemp.vx)/(obj.radiusWheel*vObjTemp.omegaW);
+            elseif strcmp(accelBrakeFlag, 'brake')
+                sigmaLat = (obj.radiusWheel*vObjTemp.omegaW - vObjTemp.vx)/(vObjTemp.vx);
+            end
+            
+            
+            sigmaLong = vObjTemp.vx/(obj.radiusWheel*vObjTemp.omegaW)*tan(vObjTemp.alpha);
+            sigma = sqrt(sigmaLat^2 + sigmaLong^2);
+            
+            % Calculate theta
+            theta = obj.C/(3*obj.mu*fzTemp);
+            
+            % Determine if tire is sliding/ total force
+            thetaPoly = 3*theta*sigma - 3*(theta*sigma)^2 + (theta*sigma)^3;
+            if thetaPoly < 1 %not sliding
+                Ft = obj.mu*fzTemp*thetaPoly;
+            else
+                Ft = fzTemp;
+            end
+            
+            % Calculate lat and long tire forces
+            fLong = sigmaLong/sigma*Ft;
+            fLat = sigmaLat/sigma*Ft;
+            
         end
     end
 end
