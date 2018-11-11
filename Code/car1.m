030c835d094a568955441dd63cbca138663d325c
Tanner Bitz <tannerbitz@gmail.com>
2018-11-10 17:58:10 -0700

    added Fiala model from Massera

--- Code/car1.m
+++ Code/car1.m
@@ -11,7 +11,8 @@ car.hcg    = .5;                                              % center of gravit
 car.wr     = .3048;                                           % wheel radius  (m)
 car.Iw     = 1;                                               % tire/wheel/drive train rotational intertia  (kg*m^2)
 car.maxT   = 205;                                             % maximum drivetrain torque    (N*m)
-car.mu     = 1;                                               % road/tire friction coefficient
+car.mu_s   = 1;                                               % kinetic road/tire friction coefficient
+car.mu_k   = 0.8;                                             % static road/tire friction coefficient
 car.C      = 120000;                                          % tire stiffness   (N/rad)
 
 car.refA   = 50.6*77.8*.0254^2;                               % aero reference area (m^2)
@@ -25,10 +26,7 @@ car.phi    = 0;  % vehicle heading relative to global X axis  [rad]
 car.vx     = 0;   % body x velocity
 car.vy     = 0;   % body y velocity
 car.omegaB = 0;   % body rotation rate
-car.omegaW_fl = 0; % front left wheel rotation rate
-car.omegaW_fr = 0; % front right wheel rotation rate
-car.omegaW_rl = 0;  % front left wheel rotation rate
-car.omegaW_rr = 0; % front right wheel rotation rate
+
 
 car.Fx = 0 ;     % total x-force on vehicle from previous interation
 car.Fy = 0;       % total y-force on vehicle from previous interation
--- Code/test.m
+++ Code/test.m
@@ -23,11 +23,11 @@ omegaW_rr = [];
 
 torque = 2000;
 for n = 1:length(time1)
-    t = time1(n)
+    t = time1(n);
     if  t > 5
         torque = .000;
     end
-    car.update(dt,0*pi/180,torque);
+    car.update(dt,5*pi/180,torque);
     car.plotCar();
     
     X = [X car.X];
@@ -36,19 +36,14 @@ for n = 1:length(time1)
     vx = [vx car.vx];
     vy = [vy car.vy];
     omegaB = [omegaB car.omegaB];
-    omegaW_fl = [omegaW_fl car.omegaW_fl];
-    omegaW_fr = [omegaW_fr car.omegaW_fr];
-    omegaW_rl = [omegaW_rl car.omegaW_rl];
-    omegaW_rr = [omegaW_rr car.omegaW_rr];
     
 end
 
 figure
 plot(time1,vx);
 hold on
-plot(time1, omegaW_rl*car.wr, '.')
 plot(time1,vy);
 
-legend('vx', 'omegaW*wr', 'vy')
+legend('vx',  'vy')
 
 
