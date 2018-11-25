41a71af2080d97617ca14d92eb6b549736fc0d27
Tanner Bitz <tannerbitz@gmail.com>
2018-11-25 14:32:16 -0700

    started quadprog symbolic functions

--- Code/TaylorEqs/error_equations_sym.m
+++ Code/TaylorEqs/error_equations_sym.m
@@ -21,7 +21,7 @@ decdtheta = simplify(diff(ec, theta));
 C_ec = [decdX;
         decdY;
         decdtheta];
-matlabFunction(C_ec, 'File', 'getCec');
+% matlabFunction(C_ec, 'File', 'getCec');
 
 %% el derivatives
 deldX = simplify(diff(el, X));
@@ -32,4 +32,63 @@ C_el = [deldX;
         deldY;
         deldtheta];
     
-matlabFunction(C_el, 'File', 'getCel')
+
+% matlabFunction(C_el, 'File', 'getCel')
+
+
+
+
+
+%% Kenny's Addition
+syms theta X Y phi vx vy omegaB  delta F_long v X0 Y0 theta0 del_dx del_dy del_dtheta dec_dx dec_dy dec_dtheta ql qc el0 ec0
+
+optVar = [theta X Y phi vx vy omegaB delta F_long v]; 
+
+%substite in decdX
+decdX = subs(decdX, X, X0);
+decdX = subs(decdX, Y, Y0);
+decdX = subs(decdX, theta, theta0);
+
+decdY = subs(decdY, X, X0);
+decdY = subs(decdY, Y, Y0);
+decdY = subs(decdY, theta, theta0);
+
+decdtheta = subs(decdtheta, X, X0);
+decdtheta = subs(decdtheta, Y, Y0);
+decdtheta = subs(decdtheta, theta, theta0);
+
+%substite in deldX
+deldX = subs(deldX, X, X0);
+deldX = subs(deldX, Y, Y0);
+deldX = subs(deldX, theta, theta0);
+
+deldY = subs(deldY, X, X0);
+deldY = subs(deldY, Y, Y0);
+deldY = subs(deldY, theta, theta0);
+
+deldtheta = subs(deldtheta, X, X0);
+deldtheta = subs(deldtheta, Y, Y0);
+deldtheta = subs(deldtheta, theta, theta0);
+
+
+
+% kennys code
+del = [deldX, deldY, deldtheta];
+dec = [decdX, decdY, decdtheta];
+
+deltaX = [X-X0; Y-Y0; theta-theta0];
+
+el2 = ql*(2*el0*del*deltaX + (del*deltaX)^2);
+ec2  = qc*(2*ec0*dec*deltaX + (dec*deltaX)^2);
+
+f = el2 + ec2 ;
+df = gradient(f,optVar);
+ddf = [gradient(df(1), optVar), gradient(df(2), optVar), gradient(df(3), optVar)];
+ddf = ddf';
+ddf = [ddf; zeros(length(optVar) - 3, length(optVar))];
+
+
+% plug in x,y,theta = 0 
+df = subs(df, X, 0);
+df = subs(df, Y, 0);
+df = subs(df, theta, 0);
--- Code/Untitled.m
+++ Code/Untitled.m
@@ -6,17 +6,17 @@ optVar = [theta X Y phi vx vy omegaB delta F_long v];
 del = [del_dx, del_dy, del_dtheta];
 dec = [dec_dx, dec_dy, dec_dtheta];
 
-deltaX = [x-x0; y-y0; theta-theta0];
+deltaX = [X-x0; Y-y0; theta-theta0];
 
 el2 = ql*(2*el0*del*deltaX + (del*deltaX)^2);
 ec2  = qc*(2*ec0*dec*deltaX + (dec*deltaX)^2);
 
 f = el2 + ec2 ;
-df = gradient(f,[x y theta]);
-ddf = [gradient(df(1),[x y theta]), gradient(df(2), [x y theta]), gradient(df(3), [x y theta])];
+df = gradient(f,optVar);
+ddf = [gradient(df(1), optVar), gradient(df(2), optVar), gradient(df(3), optVar)];
 
 
 % plug in x,y,theta = 0 
-df = subs(df, x, 0);
-df = subs(df, y, 0);
+df = subs(df, X, 0);
+df = subs(df, Y, 0);
 df = subs(df, theta, 0);
