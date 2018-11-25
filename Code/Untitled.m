840ed0da0eda918f61ca2ef744ea5facff39e275
KLozes <klozes@asu.edu>
2018-11-25 13:21:47 -0700

    untitled.m

--- Code/Untitled.m
+++ Code/Untitled.m
@@ -1,5 +1,7 @@
 clear all;
-syms x y theta  x0 y0 theta0 del_dx del_dy del_dtheta dec_dx dec_dy dec_dtheta ql qc el0 ec0
+syms theta X Y phi vx vy omegaB  delta F_long v x0 y0 theta0 del_dx del_dy del_dtheta dec_dx dec_dy dec_dtheta ql qc el0 ec0
+
+optVar = [theta X Y phi vx vy omegaB delta F_long v]; 
 
 del = [del_dx, del_dy, del_dtheta];
 dec = [dec_dx, dec_dy, dec_dtheta];
@@ -9,12 +11,12 @@ deltaX = [x-x0; y-y0; theta-theta0];
 el2 = ql*(2*el0*del*deltaX + (del*deltaX)^2);
 ec2  = qc*(2*ec0*dec*deltaX + (dec*deltaX)^2);
 
-ff = el2 + ec2;
-dff = gradient(ff,[x y theta]);
-ddff = [gradient(dff(1),[x y theta]), gradient(dff(2), [x y theta]), gradient(dff(3), [x y theta])];
+f = el2 + ec2 ;
+df = gradient(f,[x y theta]);
+ddf = [gradient(df(1),[x y theta]), gradient(df(2), [x y theta]), gradient(df(3), [x y theta])];
 
 
 % plug in x,y,theta = 0 
-dff = subs(dff, x, 0);
-dff = subs(dff, y, 0);
-dff = subs(dff, theta, 0);
+df = subs(df, x, 0);
+df = subs(df, y, 0);
+df = subs(df, theta, 0);
